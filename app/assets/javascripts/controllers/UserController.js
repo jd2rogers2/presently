654329592f37b8bc3d31e8ee91b0fe4eab578c65
jd2rogers2 <jd2rogers2@gmail.com>
2016-11-21 17:49:10 -0500

    can't figure out createItem in userCtrl

--- app/assets/javascripts/controllers/UserController.js
+++ app/assets/javascripts/controllers/UserController.js
@@ -1,26 +1,30 @@
 (function(){
   'use strict';
 
-  function UserController($scope, Auth, $stateParams, UserFactory, ItemFactory){
+  function UserController($scope, Auth, $stateParams, $state, UserFactory, ItemFactory){
     var userCtrl = this;
     userCtrl.currentUser = Auth.currentUser();
     userCtrl.viewUser = UserFactory.get({id: $stateParams.id});
 
-    $scope.new_item = {
-      items: {
-        // list_id: userCtrl.user.list.id,
-        name: 'name',
-        url: 'url',
-      }
-    };
+    // $scope.new_item = {
+    //   items: {
+    //     // list_id: Auth.currentUser().list.id,
+    //     name: 'name',
+    //     url: 'url',
+    //   }
+    // };
 
     userCtrl.createItem = function(input){
-      var new_item = ItemFactory.item = input;
-      return new_item.save();
+      // var new_item = new ItemFactory
+      input.items.list_id = userCtrl.currentUser.list.id;
+      debugger;
+      ItemFactory.save(input)
+      debugger;
+      $state.go($state.current, {}, {reload: true})
     }
   }
 
-  UserController.$inject = ['$scope', 'Auth', '$stateParams', 'UserFactory', 'ItemFactory']
+  UserController.$inject = ['$scope', 'Auth', '$stateParams', '$state', 'UserFactory', 'ItemFactory']
 
   angular
     .module('app')
