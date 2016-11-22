62d893981a4d1a1f5f8b9ba20b51f184deb705cd
jd2rogers2 <jd2rogers2@gmail.com>
2016-11-22 12:23:17 -0500

    set up listeners in navbarCtrl for log in out and register to set current user for links

--- app/assets/javascripts/controllers/NavbarController.js
+++ app/assets/javascripts/controllers/NavbarController.js
@@ -2,6 +2,25 @@
   'use strict';
 
   function NavbarController($scope, $state, Auth){
+
+    $scope.setUser = function(){
+      Auth.currentUser().then(function(data){
+        $scope.currentUser = data;
+      });
+    }
+
+    $scope.on('devise:new-registration', function(event, data){
+      $scope.setUser;
+    });
+
+    $scope.on('devise:login', function(event, data){
+      $scope.setUser;
+    });
+
+    $scope.on('devise:logout', function(event, data){
+      $scope.currentUser = {};
+    });
+
     $scope.logout = function(){
       Auth.logout();
       $state.go('login')
