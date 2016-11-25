8c0bc3384f43c289bfa3c33d02d60365b457d2cb
jd2rogers2 <jd2rogers2@gmail.com>
2016-11-25 16:14:57 -0500

    unfriend button from profile page working

--- app/assets/javascripts/controllers/UserController.js
+++ app/assets/javascripts/controllers/UserController.js
@@ -1,14 +1,20 @@
 (function(){
   'use strict';
 
-  function userController($scope, Auth){
+  function userController($scope, Auth, friendshipFactory, $state){
     var userCtrl = this;
     Auth.currentUser().then(function(data){
       $scope.currentUser = data;
     });
+
+    $scope.unfriend = function(friend){
+      var temp = {id: friend.friendship_id, friendships: {user_id: $scope.currentUser.id, friend_id: friend.id}};
+      friendshipFactory.delete(temp);
+      $state.go($state.current, {}, {reload: true});
+    }
   }
 
-  userController.$inject = ['$scope', 'Auth']
+  userController.$inject = ['$scope', 'Auth', 'friendshipFactory', '$state']
 
   angular
     .module('app')
