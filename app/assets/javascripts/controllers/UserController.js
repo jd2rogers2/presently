bc323c4ad6e4e3843b7d36610aa5dd3ea70b071c
jd2rogers2 <jd2rogers2@gmail.com>
2016-11-28 14:21:12 -0500

    removed friends list from profile.html and corresponding unfriend() from userCtrl

--- app/assets/javascripts/controllers/UserController.js
+++ app/assets/javascripts/controllers/UserController.js
@@ -6,12 +6,6 @@
     Auth.currentUser().then(function(data){
       $scope.currentUser = data;
     });
-
-    $scope.unfriend = function(friend){
-      var temp = {id: friend.friendship_id, friendships: {user_id: $scope.currentUser.id, friend_id: friend.id}};
-      friendshipFactory.delete(temp);
-      $state.go($state.current, {}, {reload: true});
-    }
   }
 
   userController.$inject = ['$scope', 'Auth', 'friendshipFactory', '$state']
