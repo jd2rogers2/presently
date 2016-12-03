1ceb0e6e42d8358649944166bd9554017211816d
jd2rogers2 <jd2rogers2@gmail.com>
2016-12-03 13:31:58 -0500

    purchase component

---
+++ app/assets/javascripts/components/purchaseComponent.js
@@ -0,0 +1,18 @@
+var purchaseComponent = {
+  bindings: {
+    obj: '='
+  },
+  controller: function($scope){
+  },
+  controllerAs: 'purchaseCompCtrl',
+  template: [
+    'buying <a ui-sref="listItemShow({item_id: purchaseCompCtrl.obj.id, list_id: purchaseCompCtrl.obj.list.id})" ng-bind="purchaseCompCtrl.obj.name"></a>',
+    'for <a ui-sref="userProfile({id: purchaseCompCtrl.obj.list.id})" ng-bind="purchaseCompCtrl.obj.list.username"></a> - '
+  ].join('')
+}
+
+// purchaseComponent.$inject = ['$scope']
+
+angular
+  .module('app')
+  .component('purchase', purchaseComponent)
