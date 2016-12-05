29af6a6e405d0ea7a19d7863177794beb3ee9274
jd2rogers2 <jd2rogers2@gmail.com>
2016-12-05 18:19:39 -0500

    image upload working in itemedit.html

--- app/assets/javascripts/controllers/ItemController.js
+++ app/assets/javascripts/controllers/ItemController.js
@@ -1,18 +1,39 @@
 (function(){
   'use strict';
 
-  function itemController($scope, $state, $stateParams, itemFactory, Auth){
+  function itemController($scope, $state, $stateParams, itemFactory, Auth, Upload){
     var itemCtrl = this;
     $scope.item = itemFactory.get({id: $stateParams.item_id}); 
 
     $scope.update = function(){
+      if ($scope.image) {
+        $scope.upload($scope.image);
+      }
       var temp = {plzrender: 'list', items: $scope.item, id: $stateParams.item_id};
       itemFactory.update(temp);
       $state.go('list', {id: $scope.item.list.id});
     }
+
+    $scope.upload = function(file){
+      Upload.upload({
+        url: 'api/v1/items/' + $scope.item.id + '.json',
+        method: 'PUT',
+        headers: { 'Content-Type': false },
+        data: {items: {image: file}},
+        sendFieldsAs: 'json'
+        // data: {file: file, 'username': $scope.username}
+        // from ngFileUpload docs
+      }).then(function(response){
+        debugger;
+        // success
+      }, function(response, a, b, c){
+        debugger;
+        // fail
+      });
+    }
   }
 
-  itemController.$inject = ['$scope', '$state', '$stateParams', 'itemFactory', 'Auth']
+  itemController.$inject = ['$scope', '$state', '$stateParams', 'itemFactory', 'Auth', 'Upload']
 
   angular
     .module('app')
