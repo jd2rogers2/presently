a5f2b1b88e2a84add6634e3c1cf57175a40c57f8
jd2rogers2 <jd2rogers2@gmail.com>
2016-12-22 17:56:43 -0500

    changed around some injector formats

--- app/assets/javascripts/routes.js
+++ app/assets/javascripts/routes.js
@@ -4,17 +4,17 @@
 
   angular
     .module('app')
-    .config(function($stateProvider, $urlRouterProvider){
+    .config([$stateProvider, $urlRouterProvider, function($stateProvider, $urlRouterProvider){
       $stateProvider
         .state('login', {
           url: '/',
           templateUrl: 'views/login.html',
           controller: 'loginController as loginCtrl',
-          onEnter: function($state, Auth){
+          onEnter: ['$state', 'Auth', function($state, Auth){
             Auth.currentUser().then(function(data){
               $state.go('list', {id: data.list.id})
             })
-          }
+          }]
         })
 
       $stateProvider
@@ -22,41 +22,41 @@
           url: '/users/:id/purchases',
           templateUrl: 'views/purchases.html',
           controller: 'purchaseController as purchaseCtrl',
-          onEnter: function($state, Auth){
+          onEnter: ['$state', 'Auth', function($state, Auth){
             if (!Auth._currentUser){
               $state.go('login')
             }
-          }
+          }]
         })
         .state('userEvents', {
           url: '/users/:id/events',
           templateUrl: 'views/events.html',
           controller: 'eventController as eventCtrl',
-          onEnter: function($state, Auth){
+          onEnter: ['$state', 'Auth', function($state, Auth){
             if (!Auth._currentUser){
               $state.go('login')
             }
-          }
+          }]
         })
         .state('userProfile', {
           url: '/users/:id',
           templateUrl: 'views/profile.html',
           controller: 'profileController as profileCtrl',
-          onEnter: function($state, Auth){
+          onEnter: ['$state', 'Auth', function($state, Auth){
             if (!Auth._currentUser){
               $state.go('login')
             }
-          }
+          }]
         })
         .state('userProfileEdit', {
           url: '/users/:id/edit',
           templateUrl: 'views/editProfile.html',
           controller: 'editProfileController as editProfCtrl',
-          onEnter: function($state, Auth){
+          onEnter: ['$state', 'Auth', function($state, Auth){
             if (!Auth._currentUser){
               $state.go('login')
             }
-          }
+          }]
         })
 
       $stateProvider
@@ -64,11 +64,11 @@
           url: '/users/:id/friends',
           templateUrl: 'views/friends.html',
           controller: 'friendshipController as friendshipCtrl',
-          onEnter: function($state, Auth){
+          onEnter: ['$state', 'Auth', function($state, Auth){
             if (!Auth._currentUser){
               $state.go('login');
             }
-          }
+          }]
         })
 
       $stateProvider
@@ -76,11 +76,11 @@
           url: '/list/:id',
           templateUrl: 'views/list.html',
           controller: 'listController as listCtrl',
-          onEnter: function($state, Auth){
+          onEnter: ['$state', 'Auth', function($state, Auth){
             if (!Auth._currentUser){
               $state.go('login')
             }
-          }
+          }]
         })
 
         $stateProvider
@@ -88,24 +88,24 @@
             url: '/list/:list_id/items/:item_id/edit',
             templateUrl: 'views/items/edit.html',
             controller: 'itemController as itemCtrl',
-            onEnter: function($state, Auth){
+            onEnter: ['$state', 'Auth', function($state, Auth){
               if (!Auth._currentUser){
                 $state.go('login')
               }
-            }
+            }]
           })
           .state('listItemShow', {
             url: '/list/:list_id/items/:item_id',
             templateUrl: 'views/items/show.html',
             controller: 'itemController as itemCtrl',
-            onEnter: function($state, Auth){
+            onEnter: ['$state', 'Auth', function($state, Auth){
               if (!Auth._currentUser){
                 $state.go('login')
               }
-            }
+            }]
           })
 
       $urlRouterProvider.otherwise('/')
-    })
+    }])
 
 }())
