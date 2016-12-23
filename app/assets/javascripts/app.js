ac0c4f4b6ef00d984cabb4034ae052dcc57661d8
jd2rogers2 <jd2rogers2@gmail.com>
2016-12-23 16:30:20 -0500

    added angular.filter and ui.bootstrap to app.js and bower, put production.rb config.assets.compile back to false

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,7 +3,7 @@
   'use strict';
 
   angular
-    .module('app', ['templates', 'ui.router', 'Devise', 'ngResource', 'ngMessages', 'infinite-scroll', 'ngFileUpload', 'angular.filter'])
+    .module('app', ['templates', 'ui.router', 'Devise', 'ngResource', 'ngMessages', 'infinite-scroll', 'ngFileUpload', 'angular.filter', 'ui.bootstrap'])
     .run(['Auth', function(Auth){
       Auth.currentUser()
     }])
